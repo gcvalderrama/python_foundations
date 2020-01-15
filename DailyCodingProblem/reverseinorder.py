85b1fd633b04f7398d3a0bf50fd89780960a117c
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2020-01-15 08:21:21 -0500

    add

--- DailyCodingProblem/reverseinorder.py
+++ DailyCodingProblem/reverseinorder.py
@@ -20,6 +20,15 @@ def pre_order(node: Node, result: list):
     pre_order(node.right, result)
 
 
+def in_order(node: Node, result: list):
+    if not node:
+        return
+
+    in_order(node.left, result)
+    result.append(node.value)
+    in_order(node.right, result)
+
+
 def recursive_pre_order(root: Node, queue: deque, height: int):
     if not queue:
         return
@@ -33,11 +42,34 @@ def recursive_pre_order(root: Node, queue: deque, height: int):
         recursive_pre_order(root.right, queue, height - 1)
 
 
+def recursive_in_order(root: Node, queue: deque, height: int):
+    if not queue:
+        return
+
+    if height > 1:
+        root.left = Node()
+        recursive_in_order(root.left, queue, height - 1)
+
+    root.value = queue.popleft()
+
+    if height > 1:
+        root.right = Node()
+        recursive_in_order(root.right, queue, height - 1)
+
+
 def reverse_pre_order(target: list):
     h = int(math.log(len(target) + 1, 2))
     queue = deque(target)
     root = Node()
     recursive_pre_order(root, queue, h)
+    return root
+
+
+def reverse_in_order(target: list):
+    h = int(math.log(len(target) + 1, 2))
+    queue = deque(target)
+    root = Node()
+    recursive_in_order(root, queue, h)
     return root
 
 
@@ -50,5 +82,15 @@ class Test(unittest.TestCase):
         pre_order(root, result)
         self.assertEqual("".join(result), "abdecfg")
 
+    def test_in_order(self):
+        target = ["d", "b", "e", "a", "f", "c", "g"]
+        root = reverse_in_order(target)
+        result = list()
+        in_order(root, result)
+        self.assertEqual("".join(result), "dbeafcg")
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 
